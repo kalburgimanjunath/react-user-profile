16520599d7d5f14028a80005a2b5ed26d74e46be
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-14 17:16:48 +0530

    1 file added

---
+++ src/data/users.js
@@ -0,0 +1,18 @@
+export const USERS = [
+  {
+    id: 1,
+    name: 'manjunath kalburgi',
+    address: '#189,Ravi nagar,hubli',
+    sex: 'male',
+    phone: '378387388377',
+    email: 'manjunath@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'ravi kalburgi',
+    address: '#189,Ravi nagar,hubli',
+    sex: 'male',
+    phone: '378387388377',
+    email: 'ravi@gmail.com',
+  },
+];

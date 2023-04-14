febd238bcb34eeaa05693f298d9dafbe010d8546
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-04-14 10:54:49 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function App() {
   return (
     <div>
       <Routes>
+        <Route path="/search" element={<Dashboard />}></Route>
+        <Route path="/profiles" element={<Dashboard />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/" exact element={<Home />}></Route>
       </Routes>
